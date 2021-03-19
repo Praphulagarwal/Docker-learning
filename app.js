691e365bbb42c286d2f83030eef17c22e29d7a46
prafulla <prafulla.a@stylework.city>
2021-03-19 11:44:34 +0530

    modified app.js2

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.delete('/goals/:id', async (req, res) => {
   }
 });
 
-console.log('testing this')
+console.log('testing this for feature docker')
 
 mongoose.connect(
   'mongodb://localhost:27017/course-goals',
